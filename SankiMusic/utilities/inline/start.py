4b5319c2056dfb28cb135fa8a1838f18c572c0a6
SANSKARI_NOBITA_XD <130442021+SanskariNobita@users.noreply.github.com>
2023-07-20 14:39:38 +0530

    Update start.py

--- SankiMusic/utilities/inline/start.py
+++ SankiMusic/utilities/inline/start.py
@@ -8,13 +8,13 @@ def start_pannel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="≽ ᴄᴏᴍᴍᴀɴᴅs ≼",
+                text="• ᴄᴏᴍᴍᴀɴᴅs •",
                 url=f"https://t.me/{BOT_USERNAME}?start=help",
             )
         ],
         [
             InlineKeyboardButton(
-                text="✮ sᴇᴛᴛɪɴɢs ✮", callback_data="settings_helper"
+                text="• sᴇᴛᴛɪɴɢs •", callback_data="settings_helper"
             )
         ],
      ]
@@ -25,26 +25,26 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
     buttons = [
         [
             InlineKeyboardButton(
-                text="ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ",
+                text="• ᴀᴅᴅ ᴍᴇ ᴛᴏ ʏᴏᴜʀ ɢʀᴏᴜᴘ •",
                 url=f"https://t.me/{BOT_USERNAME}?startgroup=true",
             )
         ],
         [
             InlineKeyboardButton(
-                text="❀⋟ ʜᴇʟᴘ ⋞❀", callback_data="settings_back_helper"
+                text="• ʜᴇʟᴘ •", callback_data="settings_back_helper"
             )
         ],
         [
             InlineKeyboardButton(
-                text="✭ ᴜᴘᴅᴀᴛᴇs ✭", url=config.SUPPORT_CHANNEL
+                text="• ᴜᴘᴅᴀᴛᴇs •", url=config.SUPPORT_CHANNEL
             ),
             InlineKeyboardButton(
-                text="✭ sᴜᴘᴘᴏʀᴛ ✭", url=config.SUPPORT_GROUP
+                text="• sᴜᴘᴘᴏʀᴛ •", url=config.SUPPORT_GROUP
             )
         ],
         [
             InlineKeyboardButton(
-                text="✮ sᴏᴜʀᴄᴇ ✮", url="https://github.com/XdityaHalder/SankiMusic"
+                text=" sᴏᴜʀᴄᴇ •", url="https://t.me/its_me_Roshan_YT"
             )
         ]
      ]
